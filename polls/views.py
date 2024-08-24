f095b57cfa018291bd89458092492db6c2c75c33
Natcha Ranchan <natcha.ran@ku.th>
2024-08-24 17:22:00 +0700

    tutorial 4: can vote, see results

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     """displays the latest few questions."""
@@ -23,9 +24,30 @@ def detail(request, question_id):
 
 def results(request, question_id):
     """displays results for a particular question."""
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html",{"question": question})
 
 def vote(request, question_id):
     """handles voting for a particular choice in a particular question."""
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # find the selected choice from form in polls/templates/polls/detail.html
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):  # didn't pick any
+        # Redisplay the question voting form and inform that they didn't select the choice
+        context = {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            }
+        # when they search for templates they alr in template dir
+        return render(request, "polls/detail.html", context)
+    else:
+        selected_choice.votes = F("votes") + 1  # add the vote to database
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data.This prevents data from being posted twice if a
+        # user hits the Back button.
+
+        # After voted redirects to the results page for the question
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
