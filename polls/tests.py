0e601a921aea576e9d846979d72616aa2fe947c0
Natcha Ranchan <natcha.ran@ku.th>
2024-09-01 00:33:15 +0700

    Testcase for is_published

--- polls/tests.py
+++ polls/tests.py
@@ -70,6 +70,7 @@ class QuestionIndexViewTests(TestCase):
         question = create_question("Past Question", days=-30)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(response.context["latest_question_list"], [question])
+        self.assertTrue(question.is_published())
 
     def test_future_question(self):
         """
@@ -101,6 +102,26 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question2, question1],
         )
+    def test_future_question2(self):
+        """
+        question with future pub date with is_published method
+        """
+        question = create_question(question_text="Future", days=1)
+        self.assertFalse(question.is_published())
+        response = self.client.get(reverse('polls:index'))
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
+        question2 = Question(question_text="",pub_date=time)
+        self.assertNotContains(response, [question.question_text,question2.question_text])
+
+    def test_default_pub_date(self):
+        """
+        Testing when pub_date attribute was not inputted
+        """
+        question = Question.objects.create(question_text='default_pub')
+        self.assertTrue(question.is_published())
+
+
+
 
 
 class QuestionDetailViewTests(TestCase):
@@ -126,3 +147,4 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(reverse("polls:detail", args=(question.id,)))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, question.question_text)
+
