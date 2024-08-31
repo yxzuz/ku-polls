b6e6e6c2b39eacb9347f79aaf95db97ff200b61a
Natcha Ranchan <natcha.ran@ku.th>
2024-08-31 12:21:32 +0700

    New Question methods

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,24 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published", default=timezone.now)
     end_date = models.DateTimeField("end date", null=True, blank=True)
 
+    def is_published(self):
+        """
+        Returns True if the current date-time is on
+        or after question’s publication date by using local date/time.
+        """
+        return timezone.localtime(timezone.now()) >= self.pub_date
+
+    def can_vote(self):
+        """
+        Returns True if voting is allowed for this question.
+        That means, the current date/time is between the pub_date and end_date.
+        If end_date is null then can vote anytime after pub_date.
+        """
+        if self.end_date is not None:
+            return self.pub_date <= timezone.now() <= self.end_date
+        return self.end_date is None
+
+
     def was_published_recently(self):
         """
         Check whether the publication date is within 24 hrs
