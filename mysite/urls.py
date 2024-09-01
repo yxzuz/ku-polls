3f868415fff49fa1060804c7ef6f2e677d3e3274
Natcha Ranchan <natcha.ran@ku.th>
2024-09-01 18:48:52 +0700

    Redirecting base url to polls/

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("",RedirectView.as_view(url="polls/", permanent=False)),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
