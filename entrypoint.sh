dbb2ebffc71a9b7ed029a64074d5339f8d3298eb
Natcha Ranchan <natcha.ran@ku.th>
2024-11-24 20:09:06 +0700

    wait until database is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+until pg_isready -h db -p 5432 -U ${DATABASE_USER} -d ${DATABASE_NAME}; do
+  echo "Waiting for database to be ready..."
+  sleep 2
+done
+
 python ./manage.py migrate
 python ./manage.py runserver 0.0.0.0:8000
