e6c8cc2a437060c3458cd553f4f1f51006fcea20
Natcha Ranchan <natcha.ran@ku.th>
2024-11-24 20:07:28 +0700

    use local host

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": config("DATABASE_NAME", default="pollsdb"),
         "USER": config("DATABASE_USER", default="pollsapp"),
         "PASSWORD": config("DATABASE_PASSWORD", default="password"),
-        "HOST": config("DATABASE_HOST", default="ku-polls-db-1"),
+        "HOST": config("DATABASE_HOST", default="localhost"),
         "PORT": config("DATABASE_PORT", default="5432")
     }
 }
